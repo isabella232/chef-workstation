4c8b3cb5757061bf66d0856d4febf4de68018332
Pranay Singh <i5singh.pranay@gmail.com>
2022-03-14 14:01:48 +0530

    handling situation when default ruby is system ruby, not worksation ruby
    Signed-off-by: Pranay Singh <i5singh.pranay@gmail.com>

--- omnibus/files/ruby-env-script/ruby-env-script.rb
+++ omnibus/files/ruby-env-script/ruby-env-script.rb
@@ -56,6 +56,10 @@ def ruby_info
   end
 end
 
+def omnibus_install?
+  File.exist?(expected_omnibus_root) && File.exist?(File.join(expected_omnibus_root, "version-manifest.json"))
+end
+
 def read_version_manifest_json
   File.read(File.join(omnibus_root, "version-manifest.json"))
 end
@@ -73,11 +77,19 @@ def gem_manifest_hash
 end
 
 def chef_ws_build_version
-  manifest_hash["build_version"]
+  if omnibus_install?
+    manifest_hash["build_version"]
+  else
+    nil
+  end
 end
 
 def chef_cli_version
-  gem_manifest_hash["chef-cli"][0]
+  if omnibus_install?
+    gem_manifest_hash["chef-cli"][0]
+  else
+    nil
+  end
 end
 
 require "json"
